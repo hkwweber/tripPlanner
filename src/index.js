032b1e9f199650ee01cc6fb38d2fc6f6d043c196
Leah Gerald <Leanator13@gmail.com>
2017-11-21 12:37:27 -0500

    working on blocking it out

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 console.log('im here look at me!!!!');
 
 const mapboxgl = require("mapbox-gl");
+const buildMarker = require("./marker");
+
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiaGt3d2ViZXIiLCJhIjoiY2phOXRuaHRmMGJycDJ3cXR5bG43ZnJ3OCJ9.9neIakt1D1GK-lPDN6sh5Q';
 
@@ -11,8 +13,12 @@ const map = new mapboxgl.Map({
   style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
 });
 
-const markerDomEl = document.createElement("div");
-markerDomEl.style.width = "32px";
-markerDomEl.style.height = "39px";
-markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
-new mapboxgl.Marker(markerDomEl).setLngLat([-74.009, 40.705]).addTo(map);
+
+const marker = buildMarker("activity", [-74.009151, 40.705086]);
+marker.addTo(map);
+
+// const markerDomEl = document.createElement("div");
+// markerDomEl.style.width = "32px";
+// markerDomEl.style.height = "39px";
+// markerDomEl.style.backgroundImage = "url(http://i.imgur.com/WbMOfMl.png)";
+// new mapboxgl.Marker(markerDomEl).setLngLat([-74.009, 40.705]).addTo(map);
